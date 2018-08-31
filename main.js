a54658c0af43fdb41538579a539e957076afc582
dongdong <286084845@qq.com>
2018-08-31 21:58:15 +0800

    canvas 1.0

---
+++ main.js
@@ -0,0 +1,94 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+// 自动设置canvas尺寸
+autoSetCanvasSize(canvas);
+
+// 监听鼠标事件
+listenToMouse(canvas);
+
+// 监听橡皮擦
+var eraserEnabled = false;
+eraser.onclick = function () {
+  // 如果eraser是true，就变成false，反之亦然
+  eraserEnabled = true;
+  actions.className = 'actions x';
+};
+brush.onclick = function () {
+    eraserEnabled = false;
+    actions.className = 'actions';
+};
+
+
+
+
+/********************* 工 具 函 数 **********************/
+
+// 自动设置canvas尺寸
+function autoSetCanvasSize(canvas) {
+  setCanvasSize();
+  window.onresize = function () {
+    setCanvasSize();
+  };
+  // 设置canvas尺寸
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth;
+    var pageHeight = document.documentElement.clientHeight;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+  }
+}
+
+// 监听鼠标事件
+function listenToMouse(canvas) {
+  var using = false;
+  var lastPoint = {x: undefined, y: undefined};
+  canvas.onmousedown = function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    using = true;
+    if  (eraserEnabled) {
+      context.clearRect(x-5, y-5, 10, 10);
+    } else {
+      lastPoint = {x: x, y: y};
+    }
+  //   drawCircle(x, y, 3);
+  };
+
+  canvas.onmousemove = function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    
+    if (!using) { return; }
+
+      if (eraserEnabled) {
+        context.clearRect(x-5, y-5, 10, 10);
+      } else {
+        var newPoint = {x: x, y: y};
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y, 5);
+        lastPoint = newPoint;
+      }
+  };
+
+  canvas.onmouseup = function () {
+    using = false;
+  };
+}
+
+// 画圆
+function drawCircle(x, y, r) {
+  context.beginPath();
+  context.arc(x, y, r, 0, Math.PI*2);
+  context.fill();  
+}
+
+// 画线
+function drawLine(a, b, c, d, width) {
+  context.beginPath();
+  context.strokeStyle = 'black';
+  context.lineWidth = width;
+  context.moveTo(a, b);
+  context.lineTo(c, d);
+  context.stroke();
+  context.closePath();
+}
